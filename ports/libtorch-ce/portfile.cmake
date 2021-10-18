fe4727726d4033a6e6cd8553c3d364731c2b3180
jaysinco <jaysinco@163.com>
2021-10-18 15:48:25 +0800

    ports: add libtorch custom edition

---
+++ ports/libtorch-ce/portfile.cmake
@@ -0,0 +1,17 @@
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://download.pytorch.org/libtorch/cu111/libtorch-win-shared-with-deps-1.9.0%2Bcu111.zip"
+    FILENAME "libtorch-win-shared-with-deps-1.9.0+cu111.zip"
+    SHA512 e7c32bf40bfd60fc0aa7aecc14c5e6fcfc2244bd7b6a1faa13b8e307792fecc910b879ac231a17116563ad9ddfd69c289ee198739531af3a6b6691c6bacdd920
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+)
+
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/libtorch)
+file(RENAME ${SOURCE_PATH}/include/ ${CURRENT_PACKAGES_DIR}/tools/libtorch/include/)
+file(RENAME ${SOURCE_PATH}/lib/ ${CURRENT_PACKAGES_DIR}/tools/libtorch/lib/)
+file(RENAME ${SOURCE_PATH}/share/ ${CURRENT_PACKAGES_DIR}/tools/libtorch/share/)
