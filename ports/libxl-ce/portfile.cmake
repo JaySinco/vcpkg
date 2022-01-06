d15a5d55a9927803a615ea221d20d393c69aa791
jaysinco <jaysinco@163.com>
2022-01-06 16:15:09 +0800

    ports: add libxl-ce

---
+++ ports/libxl-ce/portfile.cmake
@@ -0,0 +1,29 @@
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" LIBXL_BUILD_SHARED)
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "dynamic" LIBXL_CRT_SHARED)
+
+vcpkg_download_distfile(ARCHIVE
+    URLS ""
+    FILENAME "libxl-4.0.1.tar.gz"
+    SKIP_SHA512
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    NO_REMOVE_ONE_LEVEL
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DLIBXL_SHARED=${LIBXL_BUILD_SHARED}
+        -DMD=${LIBXL_CRT_SHARED}
+)
+
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(INSTALL
+    ${SOURCE_PATH}/changelog.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright
+)
