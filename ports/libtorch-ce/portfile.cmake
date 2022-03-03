2f1104b74da3c6f03639faddd21fa3ca357f6d08
jaysinco <jaysinco@163.com>
2022-03-03 19:16:29 +0800

    libtorch: use lts version

--- ports/libtorch-ce/portfile.cmake
+++ ports/libtorch-ce/portfile.cmake
@@ -1,9 +1,9 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://download.pytorch.org/libtorch/cu111/libtorch-win-shared-with-deps-1.9.0%2Bcu111.zip"
-    FILENAME "libtorch-win-shared-with-deps-1.9.0+cu111.zip"
-    SHA512 e7c32bf40bfd60fc0aa7aecc14c5e6fcfc2244bd7b6a1faa13b8e307792fecc910b879ac231a17116563ad9ddfd69c289ee198739531af3a6b6691c6bacdd920
+    URLS "https://download.pytorch.org/libtorch/lts/1.8/cu111/libtorch-win-shared-with-deps-1.8.2%2Bcu111.zip"
+    FILENAME "libtorch-win-shared-with-deps-1.8.2+cu111.zip"
+    SHA512 46706c278c0fb16118fdea42d433837d7b345c4d248f2b076b0c5b41e05688706047e71969c898f9936b90ceaddae670002932aefeebd6f54b4a40f1748f4789
 )
 
 vcpkg_extract_source_archive_ex(
